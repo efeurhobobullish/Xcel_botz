319bdcb73d8f9058eb173483c4bb60b70bc81d25
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2025-01-10 08:14:50 +0100

    Update plugins

---
+++ plugins/menu.js
@@ -0,0 +1,83 @@
+const os = require("os");
+
+const fs = require("fs");
+
+const Config = require("../config");
+
+let {
+
+  fancytext,
+
+  tlang,
+
+  tiny,
+
+  runtime,
+
+  formatp,
+
+  prefix,
+
+  cmd,
+
+  commands,
+
+} = require("../lib");
+
+const long = String.fromCharCode(8206);
+
+const readmore = long.repeat(4001);
+
+const xcel = require("../lib/plugins");
+
+const { exec } = require("child_process");
+
+const translatte = require("translatte");
+
+cmd(
+
+  {
+
+    pattern: "menu",
+
+    type: "special list",
+
+    info: "user",
+
+    dontAddCommandList: true,
+
+  },
+
+  async (message) => {
+
+    try {
+
+      let menuMessage = ` 
+
+➮ʀᴜɴᴛɪᴍᴇ - ${runtime(process.uptime())} 
+
+➮ᴅᴀᴛᴇ - ${message.date} 
+
+➮ɴᴏᴡ ᴛɪᴍᴇ - ${message.time} 
+      
+➮Oᴡɴᴇʀ - ${Config.ownername} 
+
+➮Nᴜᴍ - ${owner.split(",")[0]} 
+
+➮Mᴇᴍᴏ - ${formatp(os.totalmem() - os.freemem())} 
+
+      \n *xᴄᴇʟ_ʙᴏᴛ*\n\n ${readmore} 
+
+`.trim();
+
+      return await message.bot.sendUi(message.from, { caption: menuMessage });
+
+    } catch (error) {
+
+      await message.error(error + "\nCommand:menu", error);
+
+    }
+
+  }
+
+);
